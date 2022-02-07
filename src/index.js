4fc515d3b364dfee43e2fcd8b29e25438ae37d79
sebasje2010 <sebastian.jativa@hotmail.com>
2022-02-07 18:38:57 -0500

    commit con el resultado final, lazy loading y contador de imagenes cargadas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from "./lazy"
 
-console.log('Happy hacking :)')
+const maximum=122
+const minimum=1
+const random=()=>Math.floor(Math.random()*(maximum-minimum))+minimum
+
+const createImageNode=()=>{
+    const container=document.createElement('div')
+    container.className='p-4'
+    const imagen=document.createElement('img')
+    imagen.className='mx-auto'
+    imagen.width='320'
+    imagen.dataset.src=`https://randomfox.ca/images/${random()}.jpg`
+    const imageWrapper=document.createElement('div')
+    imageWrapper.className='bg-gray-300'
+    imageWrapper.style.minHeight='100px'
+    imageWrapper.style.display='inline-block'
+    imageWrapper.appendChild(imagen)
+    container.appendChild(imageWrapper)
+    appendedImages++
+    printLog()
+    return container
+}
+const mountNode=document.getElementById('images')
+const addButton=document.querySelector('#add')
+const cleanButton=document.querySelector('#clean')
+const addImage=()=>{
+    const newImage=createImageNode()
+    mountNode.append(newImage)
+    registerImage(newImage)
+}
+
+const cleanImages = () => {
+    [...mountNode.childNodes].forEach(child => {
+        child.remove();
+    }) 
+
+}
+addButton.addEventListener('click',addImage)
+cleanButton.addEventListener("click", cleanImages)
